62adc9e078499b0d7f75db2c68dc095abccf5d74
rubensiregar <rubbento88@gmail.com>
2024-11-05 09:08:08 +0700

    initial commit

---
+++ src/components/Card.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Image from "next/image";
+
+const Card = () => {
+    return (
+        <div className="h-80 w-60 rounded-xl bg-white">
+            <img className="bg-slate-400 w-full h-40" src="" alt="" />
+            <h1>Judul Projek</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores, nam tempore?
+            </p>
+            
+        </div>
+    );
+};
+
+export default Card;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,45 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    darkMode: ["class"],
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+    	extend: {
+    		colors: {
+    			background: 'var(--background)',
+    			foreground: 'var(--foreground)'
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		}
+    	},
+    	opacities: {
+    		'5': '.05',
+    		'10': '.10',
+    		'20': '.20',
+    		'40': '.40',
+    		'60': '.60',
+    		'80': '.80',
+    		'100': '1'
+    	},
+    	sizes: {
+    		'1': '0.25rem',
+    		'2': '0.5rem',
+    		'4': '1rem',
+    		'6': '1.5rem',
+    		'8': '2rem',
+    		'16': '4rem',
+    		'20': '5rem',
+    		'24': '6rem',
+    		'32': '8rem'
+    	}
     },
-  },
-  plugins: [],
+    plugins: [require("tailwindcss-bg-patterns"), require("tailwindcss-animate")],
 };
 export default config;
