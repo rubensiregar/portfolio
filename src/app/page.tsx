e3788e91aa2b2cec4f7cf5ce91bb7461f26adf35
rubensiregar <rubbento88@gmail.com>
2024-11-06 08:09:33 +0700

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,40 @@ import {
     CarouselNext,
     CarouselPrevious,
 } from "@/components/ui/carousel";
+import Image from "next/image";
 
 export default function Home() {
     return (
         <div className="flex flex-col gap-4">
-            <section className="h-dvh pattern-isometric pattern-teal-600 pattern-size-20">
-                <h1 className="text-9xl">Saya Ruben</h1>
-                <div className="p-12">
-                    <Carousel className="w-full max-w-sm">
+            <section className="h-dvh bg-[url('https://images.unsplash.com/photo-1730456588662-3d95038308f9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
+                <div className="flex justify-center w-full p-12">
+                    <h1 className="text-9xl text-black ">Saya Ruben</h1>
+                </div>
+                <div className="hidden size-1/4">
+                    <Image
+                        src="/nahi.jpg"
+                        width={500}
+                        height={500}
+                        alt="lalala"
+                    />
+                </div>
+                <div className="p-16 flex">
+                    <Carousel className="w-dvw ">
                         <CarouselContent>
-                            <CarouselItem className="basis-1/3">
+                            {}
+                            <CarouselItem className="basis-1/1 md:basis-1/3">
                                 <Card />
                             </CarouselItem>
-                            <CarouselItem className="basis-1/3">
+                            <CarouselItem className="basis-1/1 md:basis-1/3">
                                 <Card />
                             </CarouselItem>
-                            <CarouselItem className="basis-1/3">
+                            <CarouselItem className="basis-1/1 md:basis-1/3">
+                                <Card />
+                            </CarouselItem>
+                            <CarouselItem className="basis-1/1 md:basis-1/3">
+                                <Card />
+                            </CarouselItem>
+                            <CarouselItem className="basis-1/1 md:basis-1/3">
                                 <Card />
                             </CarouselItem>
                         </CarouselContent>
@@ -32,10 +50,6 @@ export default function Home() {
             </section>
             <section>
                 <div className="flex flex-wrap gap-4 justify-center">
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
                     <Card />
                     <Card />
                 </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,14 +2,19 @@ import React from "react";
 
 const Card = () => {
     return (
-        <div className="h-80 w-60 rounded-xl bg-white">
-            <img className="bg-slate-400 w-full h-40" src="" alt="" />
-            <h1>Judul Projek</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, nam tempore?
-            </p>
-            
+        <div className="h-80 rounded-xl bg-white">
+            <img
+                className="bg-slate-400 w-full h-40 rounded-t-xl"
+                src=""
+                alt=""
+            />
+            <div className="p-4">
+                <h1 className="font-extrabold">Judul Projek</h1>
+                <p >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Asperiores, nam tempore?
+                </p>
+            </div>
         </div>
     );
 };
